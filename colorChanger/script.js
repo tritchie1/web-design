a1df6eec058b7f7a0a54f8f283226106b5e6c914
tyler <tyler.r588@stu.nebo.edu>
2018-03-27 13:13:36 -0600

    9

---
+++ colorChanger/script.js
@@ -0,0 +1,36 @@
+var modal = document.getElementById("modal");
+var changeBtn = document.getElementById("change");
+var preview = document.getElementById("preview");
+
+function Preview() {
+	color = document.querySelector('input[name="colors"]:checked').value;
+	console.log("The color is " + color);
+	preview.style.background = color;
+}
+
+function changeColor() {
+    console.log("The color is: " + color);
+    document.body.style.background = color;
+}
+
+
+changeBtn.onclick = function() {
+	modal.style.display = "grid";
+}
+
+
+function Close() {
+    modal.style.visibility = "hidden";
+    modal.style.opacity = "0";
+    preview.style.background = "#1a1a1a";
+    document.querySelector('input[name="colors"]:checked').checked = false;
+}
+
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+    preview.style.background = "#1a1a1a";
+    document.querySelector('input[name="colors"]:checked').checked = false;
+  }
+}
